32334750cb56b7190024cc4c7ac8e90e4264c677
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-12-09 22:04:41 -0600

    actualizacion de los datos de visual stduion

---
+++ Learning.FrontEnd/Learning.FrontEnd/Controllers/AlumnoController.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Learning.FrontEnd.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learning.FrontEnd.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Alumno
+            {
+                Nombre = "Juan Carlos Santamaria",
+                UniqueId = Guid.NewGuid().ToString()
+            });
+        }
+
+        public IActionResult MultiAlumno()
+        {
+            var listaAlumnos = GenerarAlumnosAlAzar();
+
+            ViewBag.CosaDinamica = "La Monja";
+            ViewBag.Fecha = DateTime.Now;
+
+            return View("MultiAlumno", listaAlumnos);
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno
+                               {
+                                   Nombre = $"{n1} {n2} {a1}",
+                                   UniqueId = Guid.NewGuid().ToString()
+                               };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
+        }
+    }
+}
--- Learning.FrontEnd/Learning.FrontEnd/Controllers/AsignaturaController.cs
+++ Learning.FrontEnd/Learning.FrontEnd/Controllers/AsignaturaController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Learning.FrontEnd.Models;
+using System.Collections.Generic;
 
 namespace Learning.FrontEnd.Controllers
 {
@@ -8,15 +9,38 @@ namespace Learning.FrontEnd.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura{
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre="Programación"
-            };
+            return View(new Asignatura
+            {
+                Nombre = "Programación",
+                UniqueId = Guid.NewGuid().ToString()
+            });
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                };
 
             ViewBag.CosaDinamica = "La Monja";
             ViewBag.Fecha = DateTime.Now;
 
-            return View(asignatura);
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }
