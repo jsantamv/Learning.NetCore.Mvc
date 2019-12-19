a39eb79daf4c51dc2123e42781f549c4859322dd
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-12-18 20:22:56 -0600

    actualizacion de los datos

--- Learning.FrontEnd/Learning.FrontEnd/Controllers/EscuelaController.cs
+++ Learning.FrontEnd/Learning.FrontEnd/Controllers/EscuelaController.cs
@@ -1,25 +1,25 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Learning.FrontEnd.Models;
+using System.Linq;
 
 namespace Learning.FrontEnd.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
         public IActionResult Index()
         {
-            var escuela = new Escuela();
-            escuela.AñoDeCreación = 2005;
-            escuela.UniqueId = Guid.NewGuid().ToString();
-            escuela.Nombre = "Platzi School";
-            escuela.Ciudad = "Bogota";
-            escuela.Pais = "Colombia";
-            escuela.Dirección = "Avd Siempre viva";
-            escuela.TipoEscuela = TiposEscuela.Secundaria;
+            
 
             ViewBag.CosaDinamica = "La Monja";
-
+            var escuela =_context.Escuelas.FirstOrDefault();
             return View(escuela);
+        }
+
+        public EscuelaController(EscuelaContext context)
+        {
+            context = _context;
         }
     }
 }
--- Learning.FrontEnd/Learning.FrontEnd/Models/EscuelaContext.cs
+++ Learning.FrontEnd/Learning.FrontEnd/Models/EscuelaContext.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Learning.FrontEnd.Models
 {
@@ -12,11 +11,66 @@ namespace Learning.FrontEnd.Models
         public DbSet<Asignatura> Asignaturas { get; set; }
         public DbSet<Curso> Cursos { get; set; }
         public DbSet<Evaluacion> Evaluacions { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
 
 
         public EscuelaContext(DbContextOptions<EscuelaContext> options) : base(options)
         {
 
+        }
+
+        /// <summary>
+        /// Sobrescritura para el metodo de LINQ
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Esto es para sobrescribir el comportamiento de On Model Creating
+            base.OnModelCreating(modelBuilder);
+
+            var escuela = new Escuela
+            {
+                AñoDeCreación = 2005,
+                UniqueId = Guid.NewGuid().ToString(),
+                Nombre = "Platzi School",
+                Ciudad = "Bogota",
+                Pais = "Colombia",
+                Dirección = "Avd Siempre viva",
+                TipoEscuela = TiposEscuela.Secundaria
+            };
+
+            modelBuilder.Entity<Escuela>().HasData(escuela);
+
+            modelBuilder.Entity<Asignatura>().HasData(
+                            new Asignatura { Nombre = "Matemáticas", UniqueId = Guid.NewGuid().ToString() },
+                            new Asignatura { Nombre = "Educación Física", UniqueId = Guid.NewGuid().ToString() },
+                            new Asignatura { Nombre = "Castellano", UniqueId = Guid.NewGuid().ToString() },
+                            new Asignatura { Nombre = "Ciencias Naturales", UniqueId = Guid.NewGuid().ToString() },
+                            new Asignatura { Nombre = "Programación", UniqueId = Guid.NewGuid().ToString() }
+                );
+
+            //Vamos a construir un nuevas entidades de alumnos
+            //Pero el Metodo HasData() requiere que se un array y no un enumerable
+            // Por eso se tiene que convertir de una lista a  un array
+            modelBuilder.Entity<Alumno>().HasData(GenerarAlumnosAlAzar().ToArray());
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar()
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno
+                               {
+                                   Nombre = $"{n1} {n2} {a1}",
+                                   UniqueId = Guid.NewGuid().ToString()
+                               };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
         }
 
     }
